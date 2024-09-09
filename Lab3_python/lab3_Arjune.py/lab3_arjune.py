67733821f1d629e65996d3dd77803f2cbba4f4d3
rarjune7 <Ravindra.Arjune87@student.qcc.cuny.edu>
2024-09-09 12:40:44 -0400

    push 2 so it goes to my repo

--- Lab3_python/lab3_Arjune.py/lab3_arjune.py
+++ Lab3_python/lab3_Arjune.py/lab3_arjune.py
@@ -6,19 +6,25 @@ Lab 3, Python review
 print("---------> Example 1: control flow <----------- ")
 labs = int(input("Enter labs' grade: "))
 exams = int(input("Enter exams' grade: "))
-
-finalgrade = (labs + exams) / 2
 gpa = ''
+finalgrade = 0
 
 
+if (0<=labs<=100 and 0<=exams<=100):
+    finalgrade = (labs + exams) / 2
+    
 if (100>= finalgrade>=90):
     gpa = 'A'
+    
 elif (90>finalgrade>=80):
     gpa = 'B'
+    
 elif (80>finalgrade>=70):
     gpa = 'C'
+    
 elif (70>finalgrade>=60):
     gpa = 'D'
+    
 elif (60>finalgrade>=0):
     gpa = 'F'
 else:
