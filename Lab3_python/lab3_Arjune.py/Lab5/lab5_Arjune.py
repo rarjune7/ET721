c9991cfd9f4471966a12f904fdebf83ab1982a69
rarjune7 <Ravindra.Arjune87@student.qcc.cuny.edu>
2024-09-16 13:14:42 -0400

    Python Functions

---
+++ Lab3_python/lab3_Arjune.py/Lab5/lab5_Arjune.py
@@ -0,0 +1,19 @@
+"""
+Ravindra Arjune
+Sep 16, Python Functions
+"""
+
+import math
+import random
+
+# Example 1: define a function to print a message. This is a function that 
+
+def hellofunction():
+    print("Welcome to function! ")
+
+# Example 2: Function that passes a user as argument, but it doesn't
+
+def greeting(username):
+    print(f"Good Afternoon {username}")
+
+# Example 3: Function with default parameter, but it doesn't
